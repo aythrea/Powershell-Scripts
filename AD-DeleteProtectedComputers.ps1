4c06b85cfa9dc3ce2582d198da709279518fb691
aythrea <aythrea@gmail.com>
2015-06-11 07:59:34 -0500

    Disable protected AD-Computers
    Powershell Script to disable and move protected AD-Computers

---
+++ AD-DeleteProtectedComputers.ps1
@@ -0,0 +1,29 @@
+Import-Module ActiveDirectory
+$date = get-date -format "d MMM yyyy"
+$usr = $env:USERNAME
+$usr = (Get-ADUser $usr).name
+$filepath = '{0}\Logs\disabledcomputers.txt' -f $env:SystemDrive
+#new-psdrive -name addc4 -PSProvider ActiveDirectory -Server dc4-ad4 -Root //rootdse/
+
+$computers = get-content "C:\disabled computer.txt" 
+
+Foreach ($computer in $computers){
+    $computer
+    $adcomputer = Get-ADComputer $computer
+    $acls = get-acl -path ad:$adcomputer
+            foreach ($acl in $acls.access) {
+                if ($acl.IdentityReference -eq "Everyone" -and $acl.AccessControlType -eq "Deny") {
+                    write-output "Prevent accidental deletion enabled"
+                    write-output "Prevent accidental deletion being removed"`r`n
+                    $acls.removeaccessrule($acl) | out-null
+                    set-acl -path ad:$adcomputer -aclobject $acls | out-null                                   
+                 }
+
+             }
+    $ou = (Get-ADComputer $adcomputer -Properties canonicalname).canonicalname
+    Set-ADComputer $adComputer -Description ("Disabled on $date by $usr // $ou")
+    $adcomputer | disable-adaccount
+    $adcomputer | move-adobject -targetpath "OU=Disabled,OU=DataCenterDC4,OU=Servers,DC=dc4,DC=hosting,DC=infor,DC=com"
+    write-host ("$date - " + "$Computer " + "in " + "$ou " +" has been disabled by $usr")
+    ("$date - " + "$Computer " + "in " + "$ou " +" was disabled by $usr") | Out-File -FilePath $filepath -append -width 200
+    }
