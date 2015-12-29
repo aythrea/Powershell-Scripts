07d93ed6571ae1605a45d3476d5a6eb96268c752
aythrea <aythrea@gmail.com>
2015-12-29 10:42:45 -0600

    Paramaterized Mailing
    Added parameters for mail services. Also scrubbed personal information

--- AD-DisableProtectedUsers.ps1
+++ AD-DisableProtectedUsers.ps1
@@ -1,32 +1,29 @@
-Import-Module ActiveDirectory
 # Date, Userinfo, and some external information before we start the process.
-# Standard Details
+
+Import-Module ActiveDirectory
 $date = get-date -format "d MMM yyyy"
 $usr = (Get-ADUser $usr).name
 $filepath = '{0}\Logs\terms.log' -f $env:SystemDrive
 $usr = $env:USERNAME
-
+$users = get-content "C:\terms.txt"
 
 ## Global Variables
-
-# Disabled Date
+# Get the current month, add value, combine to find deletion date OU
 $disdate = get-date -f "MMM"
-# Deletion Date - Change the numeral in .addmonths to change the duration.
 $deldate = (get-date).addmonths(2).tostring('MMM')
-# Combined Dates to be used for OU placement
 $comdate = "$disdate-$deldate"
 # Disabled User destination - Requires Distinguished name
 $DisUsr = "OU=$comdate,OU=Disabled,OU=Accounts,DC=Contoso,DC=com"
-# This is the mail server or smtp relay you'll be passing through
-$mailserver = "relayserver.domain.tld"
-# expected recipient of the confirmation email
-$recipient = "someone@somwhere.tld"
+# Comment this if you don't have a deny local logon group.
+$DenyLocLog = "DenyLogonGroup"
+
+## SMTP Relay Server 
+$mailserver = "relayhost.domain.tld"
+$recAddres = "address@domain.tld"
+$destAddress = "address@domain.tld"
 
 ## The Action
-
-# Calls the list of users to be disabled. 
-$users = get-content "C:\terms.txt" 
-# Cycle through each line.
+# Calls the list of users to be disabled.
 Foreach ($user in $users){
 $user
 $aduser = Get-ADuser $user
@@ -40,20 +37,18 @@ $aduser = Get-ADuser $user
                     set-acl -path ad:$aduser -aclobject $acls | out-null                                   
                  }
              }    
-    # Housekeeping tasks - Sequential
-    # Generate Random password within the for loop PER user
-    # Store the password into memory
+    # Housekeeping tasks - Sequential: Generate and apply randomized password, Update account description move account to disabled ou, log changes
     $password = ([char[]](Get-Random -Input $(48..57 + 65..90 + 97..122) -Count 16)) -join ""
     $ou = (Get-ADuser $aduser -Properties canonicalname).canonicalname
-    # Set the aduser's password
     Set-ADAccountPassword $aduser.name -NewPassword (ConvertTo-SecureString -AsPlainText "$password" -force)
-    # set aduser's description, move the account, disable account
     Set-ADuser $aduser -Description ("Disabled on $date by $usr // $ou")
     $aduser | disable-adaccount
     $aduser | move-adobject -targetpath "$disusr"
-    write-host ("Account Disabled by $adusr" + " on $date" + " Disabled in $disdate" + " to be deleted on $deldate")
+    # Comment the next line if you don't have a deny local logon group
+    add-adgroupmember $DenyLocLog $aduser
+    # Logging and Email Confirmation
+    write-host ("Account Disabled by $usr" + " on $date" + " Disabled in $disdate" + " to be deleted on $deldate")
     ("Account Disabled by $usr" + " on $date" + " Disabled in $disdate" + " to be deleted on $deldate") | Out-File -FilePath $filepath -force -append -width 200
-    # send confirmation email-- configure $recipient and $mailserver above
-    send-mailmessage -to "$recipient" -from "donotreply@somewhere.tld" -subject "Disable Account Confirmation" -Body "Account Disabled by $usr" + " on $date" + " Disabled in $disdate" + " to be deleted on $deldate" -smtpserver $mailserver
+    send-mailmessage -to "$recaddress" -from "$destaddress" -subject "Disable Account Confirmation" -Body "Account Disabled by $usr" + " on $date" + " Disabled in $disdate" + " to be deleted on $deldate" -smtpserver $mailserver
     }
 }
